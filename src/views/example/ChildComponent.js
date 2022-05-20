ce9ac37b40573e8783a6be639e829cf35bda80a2
phuongosba <trananhphuong1187@gmail.com>
2022-05-20 21:44:13 +0700

    #14: Conditional Output

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
+// class ChildComponent extends React.Component {
+//     render() {
+//         console.log(">>>> prop:", this.props)
+//         let { school, classK, courses } = this.props;
+//         return (
+//             <>  
+//                 <div className="courseList">
+//                     {courses.map((course, index) => {
+//                         if (course.price >= 2000) {
+//                             return (<div key={course.id}>{index + 1}: {course.name} - {course.price}</div>)
+//                         }
+//                     })}
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
 class ChildComponent extends React.Component {
+    state = {
+        showCourse: false
+    }
+    handleHideShow = () => {
+        this.setState({
+            showCourse: !this.state.showCourse
+        })
+    }
     render() {
-        console.log(">>>> prop:", this.props)
-        let { school, classK, courses } = this.props;
+        let { courses } = this.props;
+        let { showCourse } = this.state
         return (
-            <>  
-                <div className="courseList">
-                    {courses.map((course, index) => {
-                        return (<div key={course.id}>{index + 1}: {course.name} - {course.price}</div>)
-                    })}
-                </div>
+            <>
+                {showCourse == false ? <div><button onClick={() => this.handleHideShow()} >show</button></div>
+                :
+                <>
+                    <div className="courseList">
+                        {courses.map((course, index) => {
+                            if (course.price >= 2000) {
+                                return (<div key={course.id}>{index + 1}: {course.name} - {course.price}</div>)                         }
+                            }
+                        )}
+                    </div>
+                    <div><button onClick={() => this.handleHideShow()}>hide</button></div>
+                </>}            
             </>
         )
     }
