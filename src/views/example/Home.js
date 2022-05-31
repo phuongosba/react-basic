690d60dc61eec50a32d06a6ca91a8c488c6a1a9f
phuongosba <trananhphuong1187@gmail.com>
2022-05-31 08:04:07 +0700

    add HOC

--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import Color from "../HOC/Color";
 
 
 class Home extends React.Component {
@@ -15,4 +15,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+export default Color(Home);
